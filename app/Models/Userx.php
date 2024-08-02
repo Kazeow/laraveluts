940bbc7d31b687b07dbbacbddd49d06af40b5de2
MustafaMstf <139692216+Kazeow@users.noreply.github.com>
2024-08-02 22:04:15 +0700

    Add files via upload

---
+++ app/Models/Userx.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Userx extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nama_use', 'email', 'level'];
+}
